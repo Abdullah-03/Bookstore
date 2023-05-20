774787e526969076e26f498eda36962da4843d42
Darana <abdullahsaud03@gmail.com>
2023-05-20 04:08:21 -0700

    "Added user signup, login, logout. Additionally new tests to verify aforementioned functionality works as intended"

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse, resolve
+
+from .views import SignupPageView
+from .forms import CustomUserCreationForm
 
 
 class CustomUserTests(TestCase):
@@ -24,3 +28,23 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignupPageTests(TestCase):
+    def setUp(self) -> None:
+        self.response = self.client.get(reverse('signup'))
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed('registration/signup.html')
+        self.assertContains(self.response, "Sign up")
+        self.assertNotContains(self.response, "joe mama")
+
+    def test_signup_form(self):
+        form = self.response.context.get("form")
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+
+    def test_signup_view(self):
+        view = resolve("/accounts/signup/")
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
