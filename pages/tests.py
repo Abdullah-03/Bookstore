3d94b12f829080f0978f181271b3fb85e6230a68
Darana <abdullahsaud03@gmail.com>
2023-05-20 06:22:21 -0700

    Added navbar using bootstrap 5, gave a facelift to login and sign up pages using crispy_forms (which in turn uses django).
    Added another static page: About, with tests to go along with it.
    
    Added the capability for django to serve static files.

--- pages/tests.py
+++ pages/tests.py
@@ -1,12 +1,14 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from pages.views import AboutPageView
 
 
 class HomePageTests(SimpleTestCase):
     def setUp(self) -> None:
         self.response = self.client.get(reverse('home'))
 
-    def test_url_exists_at_correct_location(self):
+    def test_homepage_status_code(self):
         self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
@@ -17,3 +19,27 @@ class HomePageTests(SimpleTestCase):
 
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(self.response, "ligma balls")
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self) -> None:
+        self.response = self.client.get(reverse('about'))
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'I should not be here!')
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve("/about/")
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
+        )
